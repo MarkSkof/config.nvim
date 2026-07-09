472cefd3bf0e345d6a45eb566973d9b1d57004be
Mark Škof <mark.skof5@gmail.com>
2026-07-09 21:57:26 +0200

    new plugins

--- colors/dms.lua
+++ colors/dms.lua
@@ -82,7 +82,7 @@ end
 
 if not base46.theme_tables[theme_name] or base46.theme_tables[theme_name].type ~= vim.o.background then
 	local builtin = vim.deepcopy(assert(base46.get_builtin_theme(theme_base)))
-	local harmonized = base46.theme_harmonize(builtin, "#2897eb", harmony)
+	local harmonized = base46.theme_harmonize(builtin, "#88b7ea", harmony)
 	if settings.matugenTemplateNeovimSetBackground ~= false then
 		harmonized = base46.theme_set_bg(harmonized, "#101418")
 	end
